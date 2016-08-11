0092e39a10c8b36a724cfec3f3b149020bfd9d59
Ben Baumer <ben.baumer@gmail.com>
2016-08-11 10:06:22 -0400

    updated data scraping scripts

--- R/NBAscript.R
+++ R/NBAscript.R
@@ -3,11 +3,12 @@ library(RCurl)
 
 
 seasons<-list()
-for (yyy in 1950:2014){print(yyy)
+for (yyy in 2014:2014){print(yyy)
 url<-paste("http://www.basketball-reference.com/leagues/NBA_",yyy,"_games.html?lid=header_seasons",sep="")
 print(url)
+temp <- readHTMLTable(url)
 #Just regular season data
-seasons[[as.character(yyy)]]<-readHTMLTable(url)[[1]]
+seasons[[as.character(yyy)]]<- rbind(cbind(season = yyy,  temp$games, playoffs = 0), cbind(season = yyy, temp$games_playoffs, playoffs = 1))
 }
 
 output <- do.call(rbind,seasons)
--- R/NHLscript.R
+++ R/NHLscript.R
@@ -8,9 +8,11 @@ library(RCurl)
 seasons<-list()
 for (yyy in setdiff(1950:2014, 2005)){print(yyy)
                        
-                       url<-paste("http://www.hockey-reference.com/leagues/NHL_",yyy,"_games.html",sep="")
+                       url <- paste("http://www.hockey-reference.com/leagues/NHL_",yyy,"_games.html",sep="")
+                       temp <- readHTMLTable(url)
                        #Just regular season data
-                       seasons[[as.character(yyy)]] <- cbind(season = yyy, readHTMLTable(url)[[1]])
+                       seasons[[as.character(yyy)]] <- rbind(cbind(season = yyy,  temp$games, playoffs = 0), cbind(season = yyy, temp$games_playoffs, playoffs = 1))
+                      
 }
 
 output <- do.call(rbind,seasons)
