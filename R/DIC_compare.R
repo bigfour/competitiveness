2a35acc8c78781367354d76fcc0ad8244c80a68e
MICHAEL LOPEZ <mlopez1@skidmore.edu>
2016-08-30 15:19:42 -0400

    Compare DICs

---
+++ R/DIC_compare.R
@@ -0,0 +1,18 @@
+sports <- c("mlb", "nba", "nfl", "nhl")
+library(rjags)
+source("config.R")
+get_sport <- function(sport) {
+  message(paste("reading", sport, "data..."))
+  load(file.path(mcmc_dir, paste0(sport, "_8_23_constantHFA.RData")))
+  z.null <- z
+  load(file.path(mcmc_dir, paste0(sport, "_8_23_teamHFA.RData")))
+  z.alt <- z
+  diff.dic <- diffdic(z.alt$dic, z.null$dic)
+  print(diff.dic)
+  print(sport)
+  return(list(out = diff.dic , sport = sport))
+}
+
+#Your code is so much better written than mine that I feel physical pain.  
+dat <- lapply(sports[1:4], get_sport) 
+
