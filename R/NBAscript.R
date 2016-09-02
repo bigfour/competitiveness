4e64c06f829c9fca7ff8776f30aec0253f963175
MICHAEL LOPEZ <mlopez1@skidmore.edu>
2016-09-02 14:23:22 -0400

    updated NHL script

--- R/NBAscript.R
+++ R/NBAscript.R
@@ -1,14 +1,15 @@
 library(XML)
 library(RCurl)
 
+### Note: we need updated scripts (Septemer 2016)
+
 
 seasons<-list()
-for (yyy in 1950:2015){print(yyy)
+for (yyy in 2000:2015){print(yyy)
 url<-paste("http://www.basketball-reference.com/leagues/NBA_",yyy,"_games.html?lid=header_seasons",sep="")
-print(url)
 temp <- readHTMLTable(url)
 #Just regular season data
-seasons[[as.character(yyy)]]<- rbind(cbind(season = yyy,  temp$games, playoffs = 0), cbind(season = yyy, temp$games_playoffs, playoffs = 1))
+seasons[[as.character(yyy)]]<- rbind(cbind(season = yyy,  temp$schedule, playoffs = 0), cbind(season = yyy, temp$games_playoffs, playoffs = 1))
 }
 
 output <- do.call(rbind,seasons)
