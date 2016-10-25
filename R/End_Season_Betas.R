80dc7f3d0c3e891560cb038aa0abebd84278a4ad
MICHAEL LOPEZ <mlopez1@skidmore.edu>
2016-10-25 16:31:01 -0400

    Function to make plots & get matrices

--- R/End_Season_Betas.R
+++ R/End_Season_Betas.R
@@ -104,16 +104,16 @@ betas.lastfour.all.rank <- betas.lastfour.all %>%
 
 
 
-for (seasons in 2006:2016){
-  p <- ggplot(filter(betas.lastfour.all.rank, season ==seasons), 
-              aes(x = betas, group = team)) +
-    geom_density() + facet_wrap(~sport, nrow = 2) + 
-    theme(legend.position="none") + 
-    theme_bw(12) + ggtitle("Team strength estimates") + 
-    scale_x_continuous("Beta-hat")
-  file <- paste0("~/Dropbox/Competitiveness/figures/beta.hats",seasons, ".pdf")
-  ggsave(plot = p, filename = file)
-}
+#for (seasons in 2006:2016){
+#  p <- ggplot(filter(betas.lastfour.all.rank, season ==seasons), 
+#              aes(x = betas, group = team)) +
+#    geom_density() + facet_wrap(~sport, nrow = 2) + 
+#    theme(legend.position="none") + 
+#    theme_bw(12) + ggtitle("Team strength estimates") + 
+#    scale_x_continuous("Beta-hat")
+#  file <- paste0("~/Dropbox/Competitiveness/figures/beta.hats",seasons, ".pdf")
+#  ggsave(plot = p, filename = file)
+#}
 
 betas.lastfour.all.rank %>% 
   group_by(sport, season, team) %>%
@@ -133,17 +133,106 @@ lower <- -3
 upper <- 3
 library(sfsmisc)
 
-overlap.fn <- function(team1.betas, team2.betas){
-da <- density(team1.betas, from=lower, to=upper)  #Kernal density
-db <- density(team2.betas, from=lower, to=upper)  #Kernal density
-d <- data.frame(x=da$x, a=da$y, b=db$y)
-d$w <- pmin(d$a, d$b) #intersection density  
-total <- integrate.xy(d$x, d$a) + integrate.xy(d$x, d$b)
-intersection <- integrate.xy(d$x, d$w)
-overlap <- 2 * intersection / total  # compute overlap coefficient
-return(overlap)
+### Function to get probability that one team is better than another
+overlap.fn <- function(r, c, league.exa){
+  teams <- unique(league.exa$team)
+  team1.betas <- league.exa %>% filter(team == teams[r]) %>% .[["betas"]]
+  team2.betas <- league.exa %>% filter(team == teams[c]) %>% .[["betas"]]
+ # da <- density(team1.betas, from=lower, to=upper)  #Kernal density
+ # db <- density(team2.betas, from=lower, to=upper)  #Kernal density
+ # d <- data.frame(x=da$x, a=da$y, b=db$y)
+ # d$w <- pmin(d$a, d$b) #intersection density
+ # total <- integrate.xy(d$x, d$a) + integrate.xy(d$x, d$b)
+ # intersection <- integrate.xy(d$x, d$w)
+ # overlap <- 2 * intersection / total  # compute overlap coefficient
+ # probability <- ifelse(mean(team1.betas) > mean(team2.betas), overlap, 1-overlap)
+  probability <- exp(mean(team2.betas) - mean(team1.betas))/
+    (1 + exp(mean(team2.betas) - mean(team1.betas)))
+  return(probability)
 }
 
-overlap.fn(filter(teams.ex, team == "San Antonio Spurs")$betas, 
-           filter(teams.ex, team == "Atlanta Hawks")$betas)
 
+### Function by season and sport: generates matrix of posterior probabilities
+
+posterior.probs <- function(sport, season){
+sport_ <- sport
+season_ <- season
+league.exa <- betas.lastfour.all.rank %>% filter(sport == sport_, season_ == season)
+teams <- unique(league.exa$team)
+league.mat <- matrix(nrow = length(teams), 
+                     ncol = length(teams))
+rownames(league.mat) <- teams
+colnames(league.mat) <- teams
+
+
+for (r in 1:length(teams)){
+  for (c in 1:length(teams)){
+    league.mat[r,c]  <- overlap.fn(r, c, league.exa)
+  }
+}
+
+melted_league <- melt(league.mat)
+order.teams <- melted_league %>% group_by(Var1) %>% summarise(mean.win = mean(value)) %>%
+  arrange(mean.win)
+melted_league$Var1 <- factor(melted_league$Var1, levels = order.teams$Var1)
+melted_league$Var2 <- factor(melted_league$Var2, levels = order.teams$Var1)
+title <- paste("Pr(Team 1 > Team 2)", season, sport, sep = ", ")
+title <- paste("Pr(Team 1 beats Team 2)", season, sport, sep = ", ")
+p<- ggplot(data = melted_league, aes(x=Var1, y=Var2, fill =value)) + 
+  geom_tile() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  xlab("Team 2") + ylab("Team 1") + ggtitle(title) + 
+  scale_fill_gradient(limits=c(0, 1))
+return(p)
+}
+
+mlb <- posterior.probs("mlb", 2016)
+nhl <- posterior.probs("nhl", 2016)
+nba <- posterior.probs("nba", 2016)
+nfl <- posterior.probs("nfl", 2015)
+
+mlb
+nhl
+nba
+nfl
+all <- multiplot(mlb, nhl, nba, nfl, cols=2)
+ggsave(file = "teams_play.pdf", width = 12, height = 12)
+
+
+
+
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
