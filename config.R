898c577b5c77f923a9b7b42c2e1ba3a4d2b351cc
Ben Baumer <ben.baumer@gmail.com>
2017-01-13 09:53:55 -0500

    added data_raw path to config.R

--- config.R
+++ config.R
@@ -3,18 +3,21 @@
 if (Sys.info()["user"] == "bbaumer") {
   # For Ben
   root <- "~/Dropbox/git/competitiveness"
+  data_raw <- "~/Dropbox/competitiveness/data_raw/"
   mcmc_dir <- "~/dumps/Posterior_Draws"
 }
 
 if (Sys.info()["user"] == "mlopez1") {
   # For Ben
   root <- "~/Dropbox/Compete"
+  data_raw <- "~/Dropbox/competitiveness/data_raw/"
   mcmc_dir <- "~/Dropbox/Posterior_Draws"
 }
 
 if (Sys.info()["user"] == "gregorymatthews") {
   # For Greg and Mike
   root <- "~/Dropbox/competitivenessGit"
+  data_raw <- "~/Dropbox/competitiveness/data_raw/"
   mcmc_dir <- "~/Dropbox/Posterior_Draws"
 }
 
